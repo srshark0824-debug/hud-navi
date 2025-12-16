e72a786d38d604b3da8aa479fa2d599645822417
srshark0824-debug <srshark0824@gmail.com>
2025-12-16 11:04:34 +0900

    Refactor cache handling in service worker

--- service-worker.js
+++ service-worker.js
@@ -9,24 +9,6 @@ const FILES_TO_CACHE = [
   "./icon-512.png"
 ];
 
-// インストール時にキャッシュ
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-// リクエスト時はキャッシュ優先
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
 // ================================
 // リセット処理
 // ================================
@@ -116,6 +98,26 @@ document.getElementById("listBtn").addEventListener("click", () => {
     renderWaypointList();
   }
 });
+
+
+// インストール時にキャッシュ
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// リクエスト時はキャッシュ優先
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 
 
 
