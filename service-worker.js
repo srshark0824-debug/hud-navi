aa6a0889c0957795a6cd5e982abb9f0aad068d30
srshark0824-debug <srshark0824@gmail.com>
2025-12-22 11:34:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,11 +13,18 @@ const FILES_TO_CACHE = [
 // インストール時にキャッシュ
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then(async cache => {
+      for (const file of FILES_TO_CACHE) {
+        try {
+          await cache.add(file);
+        } catch (e) {
+          console.warn("Cache skip:", file);
+        }
+      }
     })
   );
 });
+
 
 // リクエスト時はキャッシュ優先
 self.addEventListener("fetch", event => {
